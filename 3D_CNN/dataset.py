bad332e0ebd53d8419d95645b484c77522cbeb98
HillaryD <HillaryD@mit.edu>
2018-12-08 01:43:58 -0500

    made corrections to for loop with cuda

--- 3D_CNN/dataset.py
+++ 3D_CNN/dataset.py
@@ -16,11 +16,13 @@ if torch.cuda.is_available():
         mri=np.load(os.path.join(train_root,file))
         mri=mri['data']
         if i==0:
-            tensor_x=torch.from_numpy(mri[0][0]).float().to(device)
-            tensor_y=torch.from_numpy(mri[0][1]).float().to(device)
+            tensor_x=torch.from_numpy(np.expand_dims(mri[0][0],axis=0)).to(device)
+            tensor_y=torch.from_numpy(np.expand_dims(mri[0][1],axis=0)).float().to(device)
         else:
-            tensor_x=torch.stack(tensor_x,torch.from_numpy(mri[0][0]).float()).to(device)
-            tensor_y = torch.stack(tensor_y, torch.from_numpy(mri[0][1]).float()).to(device)
+            tensor_x2 = torch.from_numpy(np.expand_dims(mri[0][0], axis=0)).to(device)
+            tensor_y2 = torch.from_numpy(np.expand_dims(mri[0][1], axis=0)).to(device)
+            tensor_x = torch.cat((tensor_x, tensor_x2), dim=0).to(device)
+            tensor_y = torch.cat((tensor_y, tensor_y2.float()), dim=0).to(device)
         i+=1
 
 else:
@@ -46,7 +48,7 @@ train_dataset=utils.TensorDataset(tensor_x,tensor_y)
 
 def get_data_loaders(batch_size):
     train_loader = torch.utils.data.DataLoader(
-            train_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     return train_loader
 
 # def get_test_test_loaders(batch_size):
