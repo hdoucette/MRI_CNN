3d82c162c97c06de1f24bf85f0d4dfb52dd4095b
HillaryD <HillaryD@mit.edu>
2018-12-10 15:25:19 -0500

    fixed variable name of loss list

--- 3D_CNN/training cnn.py
+++ 3D_CNN/training cnn.py
@@ -42,7 +42,7 @@ def run():
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[7, 12], gamma=0.1)
 
     write_outputs=[]
-    loss=[]
+    epoch_loss=[]
     epoch = 1
     while epoch <= num_epochs:
         running_loss = 0.0
@@ -71,7 +71,7 @@ def run():
                     ))
                 running_loss = 0.0
                 gc.collect()
-        loss.append([epoch,running_loss])
+        epoch_loss.append([epoch,running_loss])
         gc.collect()
         # save after every epoch
         torch.save(model.state_dict(), "Model/model.%d" % epoch)
