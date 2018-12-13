684979113537783b97165f781d0a9eaa0094042c
HillaryD <HillaryD@mit.edu>
2018-12-12 19:48:39 -0500

    Change model parameters

--- 3D_CNN/CNN_Model.py
+++ 3D_CNN/CNN_Model.py
@@ -6,10 +6,10 @@ class MRI_CNN(nn.Module):
         super(MRI_CNN, self).__init__()
 
         self.features = nn.Sequential(
-            nn.Conv3d(1,16,kernel_size=(10,10,10),stride=2,padding=0),
+            nn.Conv3d(1,16,kernel_size=(8,8,8),stride=2,padding=0),
             nn.BatchNorm3d(16),
             nn.LeakyReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=(2,2,2),stride=2,padding=0),
+            nn.MaxPool3d(kernel_size=(3,3,3),stride=2,padding=0),
             nn.Conv3d(16,32,kernel_size=(4,4,4),stride=2,padding=0),
             nn.BatchNorm3d(32),
             nn.LeakyReLU(inplace=True),
--- 3D_CNN/training_cnn.py
+++ 3D_CNN/training_cnn.py
@@ -37,7 +37,7 @@ def run():
     model = MRI_CNN()
     model = model.to(device)
 
-    weights = torch.FloatTensor([1.0, 2.0, 10.0])
+    weights = torch.FloatTensor([1.0, 10.0, 20.0])
     criterion = nn.CrossEntropyLoss(weight=weights).to(device)
 
     # TODO: May Need adjustment
